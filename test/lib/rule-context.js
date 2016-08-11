56330afc478933eb416dbd4f1d06a6ac845cda54
duaraghav8@gmail <duaraghav8@gmail.com>
2016-08-11 13:24:02 +0530

    rule-context.js - tests

--- test/lib/rule-context.js
+++ test/lib/rule-context.js
@@ -7,11 +7,12 @@
 
 var should = require ('should');
 var RuleContext = require ('../../lib/rule-context'),
-	Solium = require ('../../lib/solium');
+	Solium = require ('../../lib/solium'),
+	_ = require ('lodash');
 
-describe ('Testing a new RuleContext object', function () {
+describe ('Testing RuleContext object', function () {
 
-	/*it ('should create a RuleContext instance and expose an API when valid arguments are passed', function (done) {
+	it ('should create a RuleContext instance and expose an API when valid arguments are passed', function (done) {
 		var ruleMeta = {
 			enabled: true,
 			recommended: true,
@@ -25,7 +26,26 @@ describe ('Testing a new RuleContext object', function () {
 		rcObject.should.be.type ('object');
 		rcObject.should.be.instanceOf (RuleContext);
 
+		rcObject.should.have.ownProperty ('getSourceCode');
+		rcObject.getSourceCode.should.be.type ('function');
+
+		rcObject.should.have.ownProperty ('on');
+		rcObject.on.should.be.type ('function');
+
+		rcObject.should.have.ownProperty ('report');
+		rcObject.report.should.be.type ('function');
+
+		//read-only properties
+		rcObject.should.have.ownProperty ('name');
+		rcObject.name.should.be.type ('string');
+		Object.getOwnPropertyDescriptor (rcObject, 'name').writable.should.equal (false);	//check for read-only
+
+		rcObject.should.have.ownProperty ('meta');
+		rcObject.meta.should.be.type ('object');
+		_.isEqual (rcObject.meta, ruleMeta).should.equal (true);
+		Object.getOwnPropertyDescriptor (rcObject, 'meta').writable.should.equal (false);	//check for read-only
+
 		done ();
-	});*/
+	});
 
 });
