a03e630b322ea0228cecebe61522ebb100e42d60
duaraghav8@gmail <duaraghav8@gmail.com>
2016-08-26 16:01:59 +0530

    no-with rule tests

---
+++ test/lib/rules/no-with/no-with.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Tests for no-with rule
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+'use strict';
+
+var should = require ('should');
+var Solium = require ('../../../../lib/solium'),
+	fs = require ('fs'),
+	path = require ('path');
+
+var userConfig = {
+  "custom-rules-filename": null,
+  "rules": {
+    "no-with": true,
+  }
+};
+
+describe ('[RULE] no-with: Rejections', function () {
+
+	it ('should disallow the use of "with" statement(s)', function (done) {
+		var code = 'with (myOwnObject) { /*definition*/ } with (myOwnObject2) { /*definition*/ }',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		Solium.reset ();
+		done ();
+	});
+
+});
