134e26ba3d251301fff9a5c2869b92554ef01880
duaraghav8@gmail <duaraghav8@gmail.com>
2016-08-26 11:51:57 +0530

    no-empty-blocks rule tests

---
+++ test/lib/rules/no-empty-blocks/no-empty-blocks.js
@@ -0,0 +1,171 @@
+/**
+ * @fileoverview Tests for no-empty-blocks rule
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+'use strict';
+
+var should = require ('should');
+var Solium = require ('../../../../lib/solium'),
+	fs = require ('fs'),
+	path = require ('path');
+
+var userConfig = {
+  "custom-rules-filename": null,
+  "rules": {
+    "no-empty-blocks": true,
+  }
+};
+
+describe ('[RULE] no-empty-blocks: Acceptances', function () {
+
+	it ('should accept all non-empty contract & library statements', function (done) {
+		var code = 'contract Foo { event bar (); }',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		code = 'library Foo { event bar (); }';
+		errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should accept all non-empty function declarations', function (done) {
+		var code = 'function foo () { bar (); }',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should accept all non-empty if-else declarations', function (done) {
+		var code = 'if (true) { foo (); } else { bar (); }',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+		
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should accept all non-empty for statements', function (done) {
+		var code = 'for (i = 0; i < 10; i++) { foo (); }',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+		
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should accept all non-empty do..while statements', function (done) {
+		var code = 'do { foo (); } while (i < 20);',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+		
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should accept all non-empty while statements', function (done) {
+		var code = 'while (i < 20) { bar (); }',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+		
+		Solium.reset ();
+		done ();
+	});
+
+});
+
+
+describe ('[RULE] no-empty-blocks: Rejections', function () {
+
+	it ('should reject all empty contract & library statements', function (done) {
+		var code = 'contract Foo {}',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		code = 'library Foo {}';
+		errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should reject all empty function declarations', function (done) {
+		var code = 'function foo () {}',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should reject all empty if-else declarations', function (done) {
+		var code = 'if (true) {} else {}',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+		
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should reject all empty for statements', function (done) {
+		var code = 'for (i = 0; i < 10; i++) {}',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should reject all empty do..while statements', function (done) {
+		var code = 'do {} while (i < 20);',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+		
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should reject all empty while statements', function (done) {
+		var code = 'while (i < 20) {}',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		Solium.reset ();
+		done ();
+	});
+
+});
