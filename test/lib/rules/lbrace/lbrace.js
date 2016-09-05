14d403051d10149785ad860ae1bbcc6a78efe280
duaraghav8@gmail <duaraghav8@gmail.com>
2016-09-05 09:45:58 +0530

    space btw if/while/for & parenthetic block representing conditionals

--- test/lib/rules/lbrace/lbrace.js
+++ test/lib/rules/lbrace/lbrace.js
@@ -622,4 +622,28 @@ describe ('[RULE] lbrace: Rejections', function (done) {
 		Solium.reset ();
 		done ();
 	});
+
+
+	it ('should reject control structures \'if\', \'while\', and \'for\' if there is no space between them and the parenthetic block representing their conditional.', function (done) {
+		var code = 'if(true) {}\nelse if(true) {}',
+			errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		code = 'for(;;) {}';
+		errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		code = 'while(true) {}';
+		errors = Solium.lint (code, userConfig);
+
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (1);
+
+		Solium.reset ();
+		done ();
+	});
 });
