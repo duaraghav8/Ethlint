9f36da3a82aa1b25f86c07441ba27a925bfe7a66
duaraghav8@gmail <duaraghav8@gmail.com>
2016-09-22 22:24:09 +0530

    added test for function declaration followed by comment

--- test/lib/rules/blank-lines/accept/function.sol
+++ test/lib/rules/blank-lines/accept/function.sol
@@ -17,3 +17,18 @@ contract B is A {
 
     }
 }
+
+
+contract Chumma {
+    /// @notice this function returns something important
+    /// @return description
+    function someFunction() constant returns(uint256) {
+        return 1;
+    }
+
+    /// @notice gets something else important
+    function anotherFunction() constant returns(uint256)
+    {
+        return 2;
+    }
+}
