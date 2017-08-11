b59a2a4ef3be7da699a21e54f1c8b29d313f5341
duaraghav8 <duaraghav8@gmail.com>
2017-08-11 16:27:05 +0530

    deprecated config object schema

---
+++ config/schemas/deprecated/config-v0.5.5.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview Schema of the soliumrc config deprecated in v1.0.0.
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+'use strict';
+
+// A fully qualified object for this Schema is:
+/*
+{
+	"custom-rules-filename": "~/my-life/my-rules",
+	"rules": {
+		"deprecated-suicide": false,
+		"pragma-on-top": true
+	}
+}
+*/
+
+var Schema = {
+	type: 'object',
+
+	properties: {
+		'custom-rules-filename': { type: 'string' },
+		rules: {
+			type: 'object',
+			patternProperties: {
+				'^.+$': { type: 'boolean' }
+			},
+			additionalProperties: false
+		}
+	},
+
+	required: ['rules'],
+
+	additionalProperties: false
+};
+
+
+module.exports = Schema;
