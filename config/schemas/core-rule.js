6c91f9894e33a7ed37bd77908b080665de4c1152
duaraghav8 <duaraghav8@gmail.com>
2017-08-11 15:34:15 +0530

    solium core rule object schema

---
+++ config/schemas/core-rule.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview Schema of the latest Solium core rule object.
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+'use strict';
+
+// A fully qualified object for this Schema is:
+/*
+{
+	"meta": {
+		"docs": {
+			"recommended": true,
+			"type": "error",
+			"description": "This is a rule"
+		},
+
+		"schema": [],
+
+		"fixable": "code"
+	},
+
+	"verify": function (context) {}
+}
+*/
+
+var Schema = {
+	type: 'object',
+
+	properties: {
+
+		meta: {
+			type: 'object',
+			properties: {
+
+				docs: {
+					type: 'object',
+					properties: {
+						recommended: { type: 'boolean' },
+						type: { type: 'string', enum: ['error', 'warning'] },
+						description: { type: 'string' }
+					},
+					required: ['recommended', 'type', 'description']
+				},
+
+				schema: { type: 'array' },
+
+				fixable: {
+					type: 'string', enum: ['code', 'whitespace']
+				}
+
+			},
+			required: ['docs', 'schema']
+		},
+
+		verify: true
+
+	},
+
+	required: ['meta', 'verify'],
+
+	additionalProperties: false
+};
+
+
+module.exports = Schema;
