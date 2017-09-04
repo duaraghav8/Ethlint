fafce50e3930011ffd2c8113a2ea1c97c5150d75
duaraghav8 <duaraghav8@gmail.com>
2017-09-05 00:25:44 +0530

    autofix for deprecated-suicide rule

--- test/lib/rules/deprecated-suicide/deprecated-suicide.js
+++ test/lib/rules/deprecated-suicide/deprecated-suicide.js
@@ -6,8 +6,9 @@
 'use strict';
 
 var Solium = require ('../../../../lib/solium');
-var wrappers = require ('../../../utils/wrappers');
-var toContract = wrappers.toContract;
+var wrappers = require ('../../../utils/wrappers'),
+	toContract = wrappers.toContract, toFunction = wrappers.toFunction;
+
 var userConfig = {
 	"custom-rules-filename": null,
 	"rules": {
@@ -24,6 +25,61 @@ describe ('[RULE] deprecated-suicide', function () {
 
 		errors.constructor.name.should.equal ('Array');
 		errors.length.should.equal (1);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should replace suicide by selfdestruct when fix is applied', function (done) {
+		var unfixedCode = toFunction ('suicide(0x0);'),
+			fixedCode = toFunction ('selfdestruct(0x0);');
+
+		var fixed = Solium.lintAndFix (unfixedCode, userConfig);
+
+		fixed.should.be.type ('object');
+		fixed.should.have.ownProperty ('fixedSourceCode');
+		fixed.should.have.ownProperty ('errorMessages');
+		fixed.should.have.ownProperty ('fixesApplied');
+
+		fixed.fixedSourceCode.should.equal (fixedCode);
+		fixed.errorMessages.should.be.Array ();
+		fixed.errorMessages.length.should.equal (0);
+		fixed.fixesApplied.should.be.Array ();
+		fixed.fixesApplied.length.should.equal (1);
+
+
+		unfixedCode = toFunction ('suicide (0x0);');
+		fixedCode = toFunction ('selfdestruct (0x0);');
+
+		var fixed = Solium.lintAndFix (unfixedCode, userConfig);
+
+		fixed.should.be.type ('object');
+		fixed.should.have.ownProperty ('fixedSourceCode');
+		fixed.should.have.ownProperty ('errorMessages');
+		fixed.should.have.ownProperty ('fixesApplied');
+
+		fixed.fixedSourceCode.should.equal (fixedCode);
+		fixed.errorMessages.should.be.Array ();
+		fixed.errorMessages.length.should.equal (0);
+		fixed.fixesApplied.should.be.Array ();
+		fixed.fixesApplied.length.should.equal (1);
+
+
+		unfixedCode = toContract ('function a () { suicide(0x0); }\nfunction b () { suicide(0x0); }');
+		fixedCode = toContract ('function a () { selfdestruct(0x0); }\nfunction b () { selfdestruct(0x0); }');
+
+		var fixed = Solium.lintAndFix (unfixedCode, userConfig);
+
+		fixed.should.be.type ('object');
+		fixed.should.have.ownProperty ('fixedSourceCode');
+		fixed.should.have.ownProperty ('errorMessages');
+		fixed.should.have.ownProperty ('fixesApplied');
+
+		fixed.fixedSourceCode.should.equal (fixedCode);
+		fixed.errorMessages.should.be.Array ();
+		fixed.errorMessages.length.should.equal (0);
+		fixed.fixesApplied.should.be.Array ();
+		fixed.fixesApplied.length.should.equal (2);
 
 		Solium.reset ();
 		done ();
