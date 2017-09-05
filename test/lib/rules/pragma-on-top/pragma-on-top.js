d0326b8088527ff5379fa17baf81be0bd62f3f29
duaraghav8 <duaraghav8@gmail.com>
2017-09-05 19:02:47 +0530

    autofix mechanism for rule pragma-on-top

--- test/lib/rules/pragma-on-top/pragma-on-top.js
+++ test/lib/rules/pragma-on-top/pragma-on-top.js
@@ -8,10 +8,10 @@
 var Solium = require ('../../../../lib/solium');
 
 var userConfig = {
-  "custom-rules-filename": null,
-  "rules": {
-    "pragma-on-top": true
-  }
+	"custom-rules-filename": null,
+		"rules": {
+		"pragma-on-top": true
+	}
 };
 
 
@@ -21,13 +21,13 @@ describe ('[RULE] pragma-on-top: Acceptances', function () {
 		var code = 'pragma solidity ^4.4.0;\nimport {foo} from "bar";',
 			errors = Solium.lint (code, userConfig);
 
-		errors.constructor.name.should.equal ('Array');
+		errors.should.be.Array ();
 		errors.length.should.equal (0);
 
 		code = '\n\/*Foo Bar*\/\n\npragma solidity 4.4.0;';
 		errors = Solium.lint (code, userConfig);
 
-		errors.constructor.name.should.equal ('Array');
+		errors.should.be.Array ();
 		errors.length.should.equal (0);
 
 		Solium.reset ();
@@ -43,14 +43,16 @@ describe ('[RULE] pragma-on-top: Rejections', function () {
 		var code = '\/*Hello world*\/\nimport {foo} from "bar";',
 			errors = Solium.lint (code, userConfig);
 
-		errors.constructor.name.should.equal ('Array');
+		errors.should.be.Array ();
 		errors.length.should.equal (1);
+		errors [0].message.should.equal ('No PRAGMA directive "pragma solidity <VERSION>" found at the top of file.');
 
 		code = '\/*Hello world*\/\nimport {foo} from "bar";\npragma solidity ^4.4.0;';
 		errors = Solium.lint (code, userConfig);
 
-		errors.constructor.name.should.equal ('Array');
-		errors.length.should.equal (2);
+		errors.should.be.Array ();
+		errors.length.should.equal (1);
+		errors [0].message.should.equal ('Pragma Directive "pragma solidity ^4.4.0;" should only be at the top of the file.');
 
 		Solium.reset ();
 		done ();
@@ -60,13 +62,13 @@ describe ('[RULE] pragma-on-top: Rejections', function () {
 		var code = 'pragma solidity ^4.4.0;\npragma solidity ^4.4.0;',
 			errors = Solium.lint (code, userConfig);
 
-		errors.constructor.name.should.equal ('Array');
+		errors.should.be.Array ();
 		errors.length.should.equal (1);
 
 		code = 'pragma solidity ^4.4.0;\ncontract Foo {}\npragma solidity ^4.4.0;';
 		errors = Solium.lint (code, userConfig);
 
-		errors.constructor.name.should.equal ('Array');
+		errors.should.be.Array ();
 		errors.length.should.equal (1);
 
 		Solium.reset ();
