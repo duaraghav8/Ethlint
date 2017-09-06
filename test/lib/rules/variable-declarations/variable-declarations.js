b87bef62a34fe3d987123f27ceb20b9000d95cd5
duaraghav8 <duaraghav8@gmail.com>
2017-09-06 16:40:22 +0530

    allow config option in variable-declarations rule

--- test/lib/rules/variable-declarations/variable-declarations.js
+++ test/lib/rules/variable-declarations/variable-declarations.js
@@ -1,34 +1,34 @@
 /**
- * @fileoverview Tests for no-unused-vars rule
+ * @fileoverview Tests for variable-declarations rule
  * @author Raghav Dua <duaraghav8@gmail.com>
  */
 
 'use strict';
 
-var Solium = require ('../../../../lib/solium');
-var wrappers = require ('../../../utils/wrappers');
-var toFunction = wrappers.toFunction;
+var Solium = require ('../../../../lib/solium'),
+	wrappers = require ('../../../utils/wrappers');
+var toFunction = wrappers.toFunction, toContract = wrappers.toContract;
 
 var userConfig = {
-  "custom-rules-filename": null,
-  "rules": {
-    "variable-declarations": true
-  }
+	"custom-rules-filename": null,
+		"rules": {
+		"variable-declarations": true
+	}
 };
 
 describe ('[RULE] variable-declarations: Rejections', function () {
 
 	it ('should reject all variables having names "l", "o" or "I"', function (done) {
-		var code = [
+		var declarations = [
 			'var l; var O; var I;',
 			'uint l; uint O; uint I;',
 			'string l; string O; string I;',
 			'bytes32 l; bytes32 O; bytes32 I;',
-			'mapping (uint => string) l; mapping (uint => string) O, mapping (uint => string) I;',
+			'mapping (uint => string) l; mapping (uint => string) O; mapping (uint => string) I;'
 		];
-		var errors;
+		var errors, code;
 
-		code = code.map(function(item){return toFunction(item)});
+		code = declarations.map(function(item){return toFunction(item)});
 		
 		errors = Solium.lint (code [0], userConfig);
 		errors.constructor.name.should.equal ('Array');
@@ -50,8 +50,182 @@ describe ('[RULE] variable-declarations: Rejections', function () {
 		errors.constructor.name.should.equal ('Array');
 		errors.length.should.equal (3);
 
+
+		// var cannot be used to declare state variables, so truncate it from declarations array.
+		code = declarations.slice (1).map(function(item){return toContract(item)});
+
+		errors = Solium.lint (code [0], userConfig);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (3);
+
+		errors = Solium.lint (code [1], userConfig);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (3);
+
+		errors = Solium.lint (code [2], userConfig);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (3);
+
+		errors = Solium.lint (code [3], userConfig);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (3);
+
 		Solium.reset ();
 		done ();
 	});
+
+	it ('should reject variables whose names are provided via config instead of the default names', function (done) {
+		var configWithCustomVarNames = {
+			"rules": {
+				"variable-declarations": ["error", ["myOwnVar", "myOwnVar2"]]
+			}
+		};
+
+		var declarations = [
+			'var l; var O; var I;',
+			'uint l; uint O; uint I;',
+			'string l; string O; string I;',
+			'bytes32 l; bytes32 O; bytes32 I;',
+			'mapping (uint => string) l; mapping (uint => string) O; mapping (uint => string) I;',
+
+			'var myOwnVar; var myOwnVar2;',
+			'uint myOwnVar; uint myOwnVar2;',
+			'string myOwnVar; string myOwnVar2;',
+			'bytes32 myOwnVar; bytes32 myOwnVar2;',
+			'mapping (uint => string) myOwnVar; mapping (uint => string) myOwnVar2;'
+		];
+		var errors, code;
+
+		code = declarations.map(function(item){return toFunction(item)});
+		
+		errors = Solium.lint (code [0], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [1], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [2], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [3], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [4], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [5], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [6], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [7], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [8], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [9], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+
+		// var cannot be used to declare state variables, so truncate it from declarations array.
+		code = declarations.slice (1).map(function(item){return toContract(item)});
+
+		errors = Solium.lint (code [0], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [1], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [2], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		errors = Solium.lint (code [3], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+
+		// code [4] was purposely skipped since it is var and var can't be a state variable
+		errors = Solium.lint (code [5], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [6], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [7], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		errors = Solium.lint (code [8], configWithCustomVarNames);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (2);
+
+		Solium.reset ();
+		done ();
+	});
+
+	it ('should not accept invalid values for its option', function (done) {
+		var configWithCustomVarNames = {
+			"rules": {
+				"variable-declarations": ["error", null]
+			}
+		};
+		var code = toFunction (''),
+			exceptionMessage = 'Invalid options were passed to rule "variable-declarations".';
+
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = {};
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = 0;
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = '';
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = 'hello world';
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = ['d', 10];
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = [{}];
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = [null];
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = 190.2897;
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = [-1982];
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = -1982;
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		// Empty array is a invalid option. If you need to allow all names, simply disable the rule.
+		configWithCustomVarNames.rules ['variable-declarations'] [1] = [];
+		Solium.lint.bind (Solium, code, configWithCustomVarNames).should.throw (exceptionMessage);
+
+		Solium.reset ();
+		done ();
+	})
 
 });
