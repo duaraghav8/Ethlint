a54158b7a5164265c97d558e54e89c7ef0249290
duaraghav8 <duaraghav8@gmail.com>
2018-03-29 22:27:15 +0530

    add tests for no-constant rule

--- test/lib/rules/no-constant/no-constant.js
+++ test/lib/rules/no-constant/no-constant.js
@@ -15,7 +15,23 @@ const userConfig = {
 
 describe("[RULE] emit: Acceptances", () => {
 
-    it("", done => {
+    it("should accept function declarations that don't have constant modifier", done => {
+        const declarations = [
+            "function foo() view pure returns(bool);",
+            "function foo() view pure returns(bool) {}",
+            "function foo() {}",
+            "function foo();",
+            "function(){}",
+            "function foo() myModifier(100, \"hello\") boo;"
+        ];
+
+        declarations.forEach(func => {
+            const issues = Solium.lint(`contract Foo { ${func} }`, userConfig);
+
+            issues.should.be.Array();
+            issues.should.be.empty();
+        });
+
         done();
     });
 
@@ -24,7 +40,55 @@ describe("[RULE] emit: Acceptances", () => {
 
 describe("[RULE] emit: Rejections", () => {
 
-    it("", done => {
+    it("should reject function declarations that have constant modifier", done => {
+        const declarations = [
+            "function foo() view pure constant returns(bool);",
+            "function foo() constant pure returns(bool) {}",
+            "function foo() constant {}",
+            "function foo() constant;",
+            "function()constant{}",
+            "function foo() myModifier(100, \"hello\") constant boo;"
+        ];
+
+        declarations.forEach(func => {
+            const issues = Solium.lint(`contract Foo { ${func} }`, userConfig);
+
+            issues.should.be.Array();
+            issues.should.have.size(1);
+        });
+
+        done();
+    });
+
+});
+
+
+describe("[RULE] emit: fixes", () => {
+
+    it("should replace constant with view", done => {
+        const declarations = [
+            { bad: "function foo() view pure constant returns(bool);", good: "function foo() view pure view returns(bool);" },
+            { bad: "function foo() constant pure returns(bool) {}", good: "function foo() view pure returns(bool) {}" },
+            { bad: "function foo() constant {}", good: "function foo() view {}" },
+            { bad: "function foo() constant;", good: "function foo() view;" },
+            { bad: "function()constant{}", good: "function()view{}" },
+            {
+                bad: "function foo() myModifier(100, \"hello\") constant boo;",
+                good: "function foo() myModifier(100, \"hello\") view boo;"
+            }
+        ];
+
+        declarations.forEach(({ bad, good }) => {
+            const { fixesApplied, fixedSourceCode,
+                errorMessages } = Solium.lintAndFix(`contract Foo { ${bad} }`, userConfig);
+            
+            fixesApplied.should.be.Array();
+            fixesApplied.should.have.size(1);
+            errorMessages.should.be.Array();
+            errorMessages.should.be.empty();
+            fixedSourceCode.should.equal(`contract Foo { ${good} }`);
+        });
+
         done();
     });
 
