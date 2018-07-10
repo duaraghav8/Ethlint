796d10df6c629205db5258c23e80a1733354746a
raininja <daniel.mclellan@gmail.com>
2018-07-10 18:22:15 -0400

    added fix() tests for constructor name rule

--- test/lib/rules/constructor-name/constructor-name.js
+++ test/lib/rules/constructor-name/constructor-name.js
@@ -13,27 +13,50 @@ const userConfig = {
 };
 
 describe("[RULE] constructor-name: Rejections", function() {
-    it("should correct the use of the bounty name as constructor", done => {
+    it("should identify the use of the bounty name as constructor", done => {
         let code = `
         pragma experimental "^0.5.0";
         contract Foo { function Foo() {} }
-    `;
+        `;
         const errors = Solium.lint(code, userConfig);
-        // errors.should.be.Array;
         errors.should.be.size(1);
         Solium.reset();
         done();
     });
+    it("should correct the use of bounty name as constructor", done => {
+        let unfixedCode = `
+        pragma experimental "^0.5.0";
+        contract Foo { function Foo() {} }
+        `,
+            fixedCode = `
+        pragma experimental "^0.5.0";
+        contract Foo { constructor() public {} }
+        `;
+        let fixed = Solium.lintAndFix(unfixedCode, userConfig);
+        fixed.fixedSourceCode.should.equal(fixedCode);
+        Solium.reset();
+        done();
+    });
+
 });
 describe("[RULE] constructor-name: Acceptances", function() {
     it("should accept the contract using contructor as first function", done => {
         let code = `
         pragma experimental "^0.5.0";
         contract Foo { constructor() public {} }
-    `;
+        `;
         const errors = Solium.lint(code, userConfig);
-        // errors.should.be.Array;
         errors.should.have.size(0);
+        Solium.reset();
+        done();
+    });
+    it("should not fix a correct constructor", done => {
+        let code = `
+        pragma experimental "^0.5.0";
+        contract Foo { constructor() public {} }
+        `;
+        let fixed = Solium.lintAndFix(code, userConfig);
+        fixed.fixedSourceCode.should.equal(code);
         Solium.reset();
         done();
     });
