58fb8bf26aec25ce5261080954f7c47b5f409115
Victor Porton <porton@narod.ru>
2020-01-13 08:23:32 +0200

    more tests

--- test/lib/rules/indentation/accept/assembly.sol
+++ test/lib/rules/indentation/accept/assembly.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.4.4;
 library GetCode {
     function at(address _addr) public view returns (bytes memory o_code) {
         assembly {
+            let len := mload(_data)
+
+            let data := add(_data, 0x20)
+
             for
                 { let end := add(data, mul(len, 0x20)) }
                 lt(data, end)
                 { data := add(data, 0x20) }
+            {
+                sum := add(sum, mload(data))
+            }
+
+            for { let end := add(data, mul(len, 0x20)) } lt(data, end) { data := add(data, 0x20) }
             {
                 sum := add(sum, mload(data))
             }
