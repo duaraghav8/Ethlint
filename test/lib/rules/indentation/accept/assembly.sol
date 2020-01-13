e1174e2eae4a98e1b82d438913d1f9fc70f06e4d
Victor Porton <porton@narod.ru>
2020-01-13 07:06:02 +0200

    bug fixes

--- test/lib/rules/indentation/accept/assembly.sol
+++ test/lib/rules/indentation/accept/assembly.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.4.4;
 library GetCode {
     function at(address _addr) public view returns (bytes memory o_code) {
         assembly {
-            let size := extcodesize(_addr)
-            o_code := mload(0x40)
-            mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-            mstore(o_code, size)
-            extcodecopy(_addr, add(o_code, 0x20), 0, size)
+            let len := mload(_data)
+
+            let data := add(_data, 0x20)
+
+            for
+                { let end := add(data, mul(len, 0x20)) }
+                lt(data, end)
+                { data := add(data, 0x20) }
+            {
+                sum := add(sum, mload(data))
+            }
         }
     }
 }
